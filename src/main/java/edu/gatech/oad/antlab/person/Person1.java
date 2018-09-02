e3281109179acb55706f7712e66cb2d0c569b0c2
Li Wang <liwwang@ucdavis.edu>
2018-09-02 17:52:13 -0400

    add

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Li Wang
+ *  @author Li Wang, GitHub:lwang03
  *  @version 1.1
  */
 public class Person1 {
