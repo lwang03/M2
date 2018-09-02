fc4bb0482d49ca84e77d33b4a197b8205b8103ee
Li Wang <liwwang@ucdavis.edu>
2018-09-02 16:59:58 -0400

    Li

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Li
  *  @version 1.1
  */
 public class Person1 {
@@ -29,9 +29,8 @@ public class Person1 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+  	private String calc(String input) {
+  	    return input.substring(2) + input.substring(0, 2);
 	}
 	
 	/**
