dd1a8a0bac6f0fe0187f33ad475a643181819179
Li Wang <liwwang@ucdavis.edu>
2018-09-06 16:18:49 -0400

    some small changes

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,7 +30,8 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      return input.substring(2) + input.substring(0, 2);
+
+      return input.substring(2) + input.substring(0, 2) + " ";
     }
     
     /**
